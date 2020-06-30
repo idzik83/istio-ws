e795e492e25565d8f601cc001205f0d8affeaf27
Igor Idzikovskyi <igor.idzikovskyi@petuum.com>
2020-06-30 15:17:14 +0300

    add content-type

--- istio/quote-service/server/views.py
+++ istio/quote-service/server/views.py
@@ -10,7 +10,7 @@ QUOTE_SERVER_URL = os.getenv('QUOTE_SERVER_URL', 'https://type.fit/api/quotes')
 
 async def get_random_quote(request):
     response = await requests.get(QUOTE_SERVER_URL)
-    quotes = await response.json()
+    quotes = await response.json(content_type='text/plain')
     rand_quote_idx = random.randrange(len(quotes))
     quote = quotes[rand_quote_idx]
     return web.json_response({'quote': quote['text'], 'author': quote['author']})
